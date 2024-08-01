83e15bbfed5fab3a1e0112b87bd2d4567a59a553
Adar Dagan <AdarDagan@outlook.com>
2024-08-01 11:32:15 +0300

    day 6 part 2

--- day06.cpp
+++ day06.cpp
@@ -20,55 +20,59 @@ int main() {
   }
   for (; *distanceIter != ' '; ++distanceIter) {
   }
+  for (; *timeIter == ' '; ++timeIter) {
+  }
+  for (; *distanceIter == ' '; ++distanceIter) {
+  }
 
   auto margin = 1;
-  while (timeIter != timeLine.end() && distanceIter != distanceLine.end()) {
-    for (; *timeIter == ' '; ++timeIter) {
-    }
-    for (; *distanceIter == ' '; ++distanceIter) {
-    }
 
-    int time = 0;
-    for (; timeIter != timeLine.end() && *timeIter != ' '; ++timeIter) {
-      time = time * 10 + (*timeIter - '0');
+  long long time = 0;
+  for (; timeIter != timeLine.end(); ++timeIter) {
+    if (*timeIter == ' ') {
+      continue;
     }
-    int distance = 0;
-    for (; distanceIter != distanceLine.end() && *distanceIter != ' ';
-         ++distanceIter) {
-      distance = distance * 10 + (*distanceIter - '0');
+    time = time * 10 + (*timeIter - '0');
+  }
+  long long distance = 0;
+  for (; distanceIter != distanceLine.end(); ++distanceIter) {
+    if (*distanceIter == ' ') {
+      continue;
     }
+    distance = distance * 10 + (*distanceIter - '0');
+  }
 
-    auto disc = time * time - 4 * distance;
-    if (disc < 0) {
+  auto disc = time * time - 4 * distance;
+  cout << disc << endl;
+  if (disc < 0) {
+    cout << "race with no way to win" << endl;
+    margin = 0;
+    return 1;
+  }
+
+  if (disc == 0) {
+    if (time % 2 == 0) {
+      return 1;
+    } else {
       cout << "race with no way to win" << endl;
       margin = 0;
-      break;
+      return 1;
     }
-
-    if (disc == 0) {
-      if (time % 2 == 0) {
-        continue;
-      } else {
-        cout << "race with no way to win" << endl;
-        margin = 0;
-        break;
-      }
-    }
-
-    auto temp = (time - sqrt(disc)) / 2.0;
-    auto root1 = ceil(temp);
-    if (root1 == temp) {
-      root1 += 1;
-    }
-    temp = (time + sqrt(disc)) / 2.0;
-    auto root2 = floor(temp);
-    if (root2 == temp) {
-      root2 -= 1;
-    }
-
-    cout << root1 << " " << root2 << " " << root2 - root1 + 1 << endl;
-    margin *= (root2 - root1) + 1;
   }
+
+  auto temp = (time - sqrt(disc)) / 2.0;
+  auto root1 = ceil(temp);
+  if (root1 == temp) {
+    root1 += 1;
+  }
+  temp = (time + sqrt(disc)) / 2.0;
+  auto root2 = floor(temp);
+  if (root2 == temp) {
+    root2 -= 1;
+  }
+
+  cout << root1 << " " << root2 << " " << root2 - root1 + 1 << endl;
+  margin *= (root2 - root1) + 1;
 
   cout << margin << endl;
 }
